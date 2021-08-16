8289058dec206e1803c4675b955992049258eb5d
JunKi, Hong <dylanhong920509@gmail.com>
2021-08-16 21:06:46 +0900

    03 Event Driven Programming

--- config.cmake
+++ config.cmake
@@ -27,8 +27,8 @@ list(APPEND LIB_LIST "SDL2main")
 # source lists
 # ===================================
 # list(APPEND SRC_LIST "01_HelloSDL/main.cpp")
-list(APPEND SRC_LIST "02_GettingAnImageOnTheScreen/main.cpp")
-# list(APPEND SRC_LIST "03_EventDrivenProgramming/main.cpp")
+# list(APPEND SRC_LIST "02_GettingAnImageOnTheScreen/main.cpp")
+list(APPEND SRC_LIST "03_EventDrivenProgramming/main.cpp")
 # list(APPEND SRC_LIST "04_KeyPresses/main.cpp")
 # list(APPEND SRC_LIST "05_OptimizedSurfaceLoadingAndSoft/main.cpp")
 
