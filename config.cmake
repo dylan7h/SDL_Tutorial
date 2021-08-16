19c1cc4f8e87c78a339d9ebe16fc686068fd27da
JunKi, Hong <dylanhong920509@gmail.com>
2021-08-16 20:40:29 +0900

    project configuration

---
+++ config.cmake
@@ -0,0 +1,34 @@
+# ===================================
+# defines
+# ===================================
+list(APPEND DEF_LIST "_UNICODE")
+list(APPEND DEF_LIST "UNICODE")
+
+# ===================================
+# include paths
+# ===================================
+list(APPEND INC_PATH "C:/Program Files/SDL2-2.0.16/include")
+
+
+# ===================================
+# library paths
+# ===================================
+list(APPEND LIB_PATH "C:/Program Files/SDL2-2.0.16/lib/x64")
+
+
+# ===================================
+# library lists
+# ===================================
+list(APPEND LIB_LIST "SDL2")
+list(APPEND LIB_LIST "SDL2main")
+
+
+# ===================================
+# source lists
+# ===================================
+# list(APPEND SRC_LIST "01_HelloSDL/main.cpp")
+list(APPEND SRC_LIST "02_GettingAnImageOnTheScreen/main.cpp")
+# list(APPEND SRC_LIST "03_EventDrivenProgramming/main.cpp")
+# list(APPEND SRC_LIST "04_KeyPresses/main.cpp")
+# list(APPEND SRC_LIST "05_OptimizedSurfaceLoadingAndSoft/main.cpp")
+
